338723c26236376eedc97bdd9c511f044ead9fde
Sanket Mehta <sanket.work7@gmail.com>
2019-04-02 18:12:37 +0530

    v3

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+var responseData;
 define(function (require) {
     var Color = require('./color');	
     var colorList = require('./color-list');	
@@ -110,7 +111,7 @@ define(function (require) {
 			  '@primary': maincolor,
 			  '@primaryLow': priLow.toCSS(),
 			  '@primaryHigh': priHigh.toCSS(),
-			  '@accent': window.accent,
+			  '@accent': window.accent?window.accent:"#fff",
 			  '@accentLow': window.accentLow,
 			  '@accentTint': window.accenttint,
 			  '@accentShade': window.accentshade
@@ -193,8 +194,10 @@ define(function (require) {
 			clip($("#cssmodel pre")[0]);
 	})
 	$("cross").on(myclick,function(){
-		$(".modelBack").hide();
-		$(".model").hide();
+		$('#cssmodel').modal('hide');
+	})
+	$(".codeBtn").on(myclick,function(){
+		$('#cssmodel').modal({backdrop:false});
 	})
 	$(".burger").on(myclick,function(){
 		if($(".nav ul.menuMobile").is(":visible")){
@@ -219,5 +222,344 @@ define(function (require) {
 		$("#Purple_600").trigger(myclickUp);
 		$("#Orange_A700").trigger(myclickUp);
 	},1000)
+
 	
+	
+	function loadClient() {
+		var apiKey="AIzaSyAnJQ7taZsJbPRnGKEH06ub-ObCg-RlQ7Y";
+	    gapi.client.setApiKey(apiKey);
+	    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/webfonts/v1/rest")
+	        .then(function() { console.log("GAPI client loaded for API"); 
+	    					 execute();
+	    					},
+	              function(err) { console.error("Error loading GAPI client for API", err); });
+
+	  }
+	  // Make sure the client is loaded before calling this method.
+	  function execute() {
+	    return gapi.client.webfonts.webfonts.list({
+	      "sort": "popularity"
+	    })
+	        .then(function(response) {
+	                // Handle the results here (response.result has the parsed body).
+	                //console.log("Response", response);
+	                responseData=response;
+	                loadExtraFonts();
+	                fetchFontNames();
+	              },
+	              function(err) { console.error("Execute error", err); });
+	  }
+	  gapi.load("client");
+
+	  setTimeout(function(){
+			loadClient();
+			
+		},5000)
+
+
+    //   <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+    //   <label class="custom-control-label" for="customCheck">Custom checkbox</label>
+    // </div>
+		function fetchFontNames(){
+			var s1='<div class="custom-control custom-checkbox"><div class="checkContent">'
+					+'<input id="' 
+			var s2=	'" type="checkbox" class="custom-control-input" onchange="doalert(this.id)" name="example1">'
+					+'<label class="custom-control-label" for="'
+			var s3='">'
+			var s4='</label></div></div>'
+			var fString="";
+			var categoryArr=[];
+
+			responseData.result.items.forEach(function(alone){
+				if(categoryArr.indexOf(alone.category)==-1){
+			    	categoryArr.push(alone.category)
+				}
+			})
+
+			categoryArr.forEach(function(alone){
+				fString=fString+s1+alone+s2+alone+s3+alone+s4;
+			})
+
+			document.querySelector(".checkBoxParent").innerHTML=fString;
+
+
+			var finalString='';
+			var appendString1='<div class="fontName"><div class="custom-control custom-radio"><input class="custom-control-input" onchange="radioCallPrimary(this.id.split(\'-\')[1])"  type="radio" name="customRadio123" id="customIdRadio1-';
+			var appendString2='" value="option1"><label class="custom-control-label" style="font-family:\''
+			var appendString3='\'" for="customIdRadio1-';
+			var appendString4='">';
+			var appendString5='</label></div></div>'
+			responseData.result.items.forEach(function(alone){
+				finalString=finalString+appendString1+alone.family+appendString2+alone.family+appendString3+alone.family+appendString4+alone.family+appendString5;
+			})
+
+			document.querySelector(".fontsParent1").innerHTML=finalString;
+			var t1 = finalString.replace(/customRadio123/g,"customRadio234");
+			t1 = t1.replace(/customIdRadio1/g,"customIdRadio2");
+			t1 = t1.replace(/radioCallPrimary/g,"radioCallSecondary")
+			document.querySelector(".fontsParent2").innerHTML=t1;
+
+			var finalDropString="";
+			var dropString1='<a class="dropdown-item pimaryList" onclick="radioCallPrimary(this.id)" style="font-family:\'';
+			var dropString2='\'" id="';
+			var dropString3='">';
+			var dropString4='</a>';
+			responseData.result.items.forEach(function(alone){
+				finalDropString=finalDropString+dropString1+alone.family+dropString2+alone.family+dropString3+alone.family+dropString4;
+			})
+
+			document.querySelector('.primaryDrop .dropdown-menu').innerHTML=finalDropString;
+			var t2=finalDropString.replace(/pimaryList/g,"secondaryList");
+			t2=t2.replace(/radioCallPrimary/g,"radioCallSecondary");
+			document.querySelector('.secondaryDrop .dropdown-menu').innerHTML=t2;
+
+		}
+
 });
+
+var fontNameArray=[];
+var firstFontName=[];
+var secondFontArray=[];
+var progress=0;
+function loadExtraFonts(){
+	responseData.result.items.forEach(function(alone){
+		fontNameArray.push(alone.family)
+	});
+
+	for(var i=0;i<10;i++){
+		firstFontName.push(fontNameArray[i]);
+	}
+	for(var i=10;i<fontNameArray.length;i=i){
+		var temp=[];
+		for(var j=0;j<25;j++){
+			if(fontNameArray[i])
+				temp.push(fontNameArray[i]);
+			i++;
+		}
+		secondFontArray.push(temp);
+	}
+	progress=progress+10;
+	
+	downloadFonts(firstFontName);
+}
+
+function downloadFonts(Arr){
+	console.log(Arr);
+	WebFont.load({
+    google: {
+      families: Arr
+    },
+    classes: false
+    // fontactive: function(familyName, fvd) {
+    // 	console.log(familyName)
+    // },
+  });
+
+}
+var activeIndex=0;
+function downFonts(firstLoad){
+	if(firstLoad){
+		$('.sliderFont').unslider();
+	}
+	
+	for(var i=0;i<9;i++){
+		setTimeout(function(){
+			downloadFonts(secondFontArray[activeIndex]);
+			activeIndex++;
+		},100)
+		
+		
+	}
+}
+var checkedArray=[];
+function doalert(id){
+	var tempId;
+	console.log(id);
+	document.querySelector('.FilterName').innerHTML=id;
+	var finalString='';
+	var appendString1='<div class="fontName"><div class="custom-control custom-radio"><input class="custom-control-input" onchange="radioCallPrimary(this.id.split(\'-\')[1])" type="radio" name="customRadio123" id="customIdRadio1-';
+	var appendString2='" value="option1"><label class="custom-control-label" style="font-family:\''
+	var appendString3='\'" for="customIdRadio1-';
+	var appendString4='">';
+	var appendString5='</label></div></div>'
+	if(document.getElementById(id).checked){
+		checkedArray.push(id);
+		checkedArray.forEach(function(aln){
+			responseData.result.items.forEach(function(alone){
+				if(alone.category==aln){
+					finalString=finalString+appendString1+alone.family+appendString2+alone.family+appendString3+alone.family+appendString4+alone.family+appendString5;
+				}
+			})
+		})
+		document.querySelector(".fontsParent1").innerHTML="";
+		document.querySelector(".fontsParent2").innerHTML="";
+		
+		document.querySelector(".fontsParent1").innerHTML=finalString;
+		var t1 = finalString.replace(/customRadio123/g,"customRadio234");
+		t1 = t1.replace(/customIdRadio1/g,"customIdRadio2");
+		t1 = t1.replace(/radioCallPrimary/g,"radioCallSecondary")
+		document.querySelector(".fontsParent2").innerHTML=t1;
+		
+	}else{
+		tempId=checkedArray.indexOf(id);
+		checkedArray.splice(tempId,1);
+		finalString="";
+		if(checkedArray.length!=0){
+			checkedArray.forEach(function(aln){
+				responseData.result.items.forEach(function(alone){
+					if(alone.category==aln ){
+						finalString=finalString+appendString1+alone.family+appendString2+alone.family+appendString3+alone.family+appendString4+alone.family+appendString5;
+					}
+				})
+			})
+		}else{
+			responseData.result.items.forEach(function(alone){
+				finalString=finalString+appendString1+alone.family+appendString2+alone.family+appendString3+alone.family+appendString4+alone.family+appendString5;
+			})
+		}
+		
+		document.querySelector(".fontsParent1").innerHTML="";
+		document.querySelector(".fontsParent2").innerHTML="";
+		
+		document.querySelector(".fontsParent1").innerHTML=finalString;
+		var t1 = finalString.replace(/customRadio123/g,"customRadio234");
+		t1 = t1.replace(/customIdRadio1/g,"customIdRadio2");
+		t1 = t1.replace(/radioCallPrimary/g,"radioCallSecondary")
+		document.querySelector(".fontsParent2").innerHTML=t1;
+	}	
+}
+function scrollCall(ele){
+	var sHeight=document.querySelector("."+ele.className).scrollHeight;
+	var sTop=document.querySelector("."+ele.className).scrollTop;
+	var sTopPercentage=(sTop*100)/sHeight;
+	if(sTopPercentage<=50){
+		if(progress<460){
+			//second 9
+			activeIndex = 9;
+			progress=progress+(25*9);
+			console.log("second 9",sTopPercentage,progress)
+			downFonts();
+		}
+	}else if(sTopPercentage<=65 && sTopPercentage>50){
+		if(progress<685){
+			//third 9
+			activeIndex = 18;
+			progress=progress+(25*9);
+			console.log("third 9",sTopPercentage)
+			downFonts();
+		}
+	}else if(sTopPercentage<=100 && sTopPercentage>65){
+		if(progress<904){
+			//last 9
+			console.log("last 9",sTopPercentage,progress)
+			activeIndex = 27;
+			progress=progress+(25*9);
+			console.log("last 9",sTopPercentage)
+			downFonts();
+		}
+	}
+}
+var currentSecondaryFont="Titillium Web";
+var currentPrimaryFont="Titillium Web";
+function radioCallPrimary(eleId){
+	
+	currentPrimaryFont=eleId;
+	less.modifyVars({
+			  '@primaryFont': currentPrimaryFont, 
+			  '@secondaryFont': currentSecondaryFont,
+			  '@primary': window.primary,
+			  '@primaryLow': window.priLow,
+			  '@primaryHigh': window.priHigh,
+			  '@accent': window.accent?window.accent:"#fff",
+			  '@accentLow': window.accentLow?window.accentLow:"#fff",
+			  '@accentTint': window.accenttint,
+			  '@accentShade': window.accentshade
+	});
+	document.querySelector('.currentPfont').innerHTML=currentPrimaryFont;
+}
+function radioCallSecondary(eleId){
+	
+	currentSecondaryFont=eleId;
+	less.modifyVars({
+			  '@primaryFont': currentPrimaryFont,  
+			  '@secondaryFont': currentSecondaryFont, 
+			  '@primary': window.primary,
+			  '@primaryLow': window.priLow,
+			  '@primaryHigh': window.priHigh,
+			  '@accent': window.accent?window.accent:"#fff",
+			  '@accentLow': window.accentLow?window.accentLow:"#fff",
+			  '@accentTint': window.accenttint,
+			  '@accentShade': window.accentshade
+	});
+	document.querySelector('.currentSfont').innerHTML=currentSecondaryFont;
+}
+function displayFilters(){
+
+	if(!document.querySelector('.checkBoxParent').classList.contains('filterHidden') && 
+		!document.querySelector('.checkBoxParent').classList.contains('filterShow')){
+
+		document.querySelector('.checkBoxParent').classList.add("filterHidden");
+
+		document.querySelector('.plusBtn').style.backgroundImage="url('../img/plus.png')";
+
+		document.querySelector('.fontsParent1').classList.add("minusFilter");
+		document.querySelector('.fontsParent2').classList.add("minusFilter");
+		
+	}else if(document.querySelector('.checkBoxParent').classList.contains('filterHidden')){
+		
+		document.querySelector('.checkBoxParent').classList.remove("filterHidden");
+
+		document.querySelector('.checkBoxParent').classList.add("filterShow");
+		document.querySelector('.plusBtn').style.backgroundImage="url('../img/minus.png')";
+
+		document.querySelector('.fontsParent1').classList.remove("minusFilter");
+		document.querySelector('.fontsParent2').classList.remove("minusFilter");
+
+		document.querySelector('.fontsParent1').classList.add("plusFilter");
+		document.querySelector('.fontsParent2').classList.add("plusFilter");
+		
+	}else if(document.querySelector('.checkBoxParent').classList.contains('filterShow')) {
+
+		document.querySelector('.checkBoxParent').classList.remove("filterShow");
+
+		document.querySelector('.checkBoxParent').classList.add("filterHidden");
+		document.querySelector('.plusBtn').style.backgroundImage="url('../img/plus.png')";
+
+
+		document.querySelector('.fontsParent1').classList.remove("plusFilter");
+		document.querySelector('.fontsParent2').classList.remove("plusFilter");
+
+		document.querySelector('.fontsParent1').classList.add("minusFilter");
+		document.querySelector('.fontsParent2').classList.add("minusFilter");
+
+	}else{
+
+	}
+	
+}
+function displaySideMenu(){
+	//document.querySelector('.sectionLeft').style.marginLeft
+	if(!document.querySelector('.sectionLeft').classList.contains('hideMenu') &&
+		!document.querySelector('.sectionLeft').classList.contains('showMenu')){
+
+		document.querySelector('.sectionLeft').classList.add("hideMenu");
+
+		document.querySelector('.sectionRight').style.width='100%'
+
+	}else if(document.querySelector('.sectionLeft').classList.contains('hideMenu')){
+
+		document.querySelector('.sectionLeft').classList.remove("hideMenu");
+		document.querySelector('.sectionLeft').classList.add('showMenu');
+
+		document.querySelector('.sectionRight').style.width='80%'
+
+	}else if(document.querySelector('.sectionLeft').classList.contains('showMenu')){
+
+		document.querySelector('.sectionLeft').classList.remove('showMenu');
+		document.querySelector('.sectionLeft').classList.add('hideMenu');
+
+		document.querySelector('.sectionRight').style.width='100%'
+	}else{
+
+	}
+}
